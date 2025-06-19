49a33add6b922324a2ab985cf51ca34279e27faa
Suvakanta Rout <suvakantarout2020@gmail.com>
2025-06-19 21:44:15 +0530

    event handeling

--- react-counter/react-counter/src/components/Counter.jsx
+++ react-counter/react-counter/src/components/Counter.jsx
@@ -4,10 +4,15 @@ import './Counter.css'
 const counter = () => {
   const [count, setCount] = useState(0);
 
+  // This function is updating the state variable (useState())
+  function increaseCount(){
+      setCount(count + 1);
+  }
+  
   return (
     <div className='counter-container'>
         <p id='para'>You have clicked {count} time</p>
-        <button id='btn' onClick={() => { setCount(count+1) }}>Click me</button>  {/* setCount(count+1) is a function which increases the count by 1 on each click of count button */}
+        <button id='btn' onClick={increaseCount}>Click me</button>  {/* setCount(count+1) is a function which increases the count by 1 on each click of count button */}
     </div>
   )
 }
