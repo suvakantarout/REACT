00ce7b274b81c97749caa8002f595bf69688811c
Suvakanta Rout <suvakantarout2020@gmail.com>
2025-06-21 22:27:37 +0530

    .

--- react-project-three/react-project-three/src/App.jsx
+++ react-project-three/react-project-three/src/App.jsx
@@ -3,24 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from "./components/Card"
+import Button from "./components/Button"
 
 function App() {
-const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-function handelClick(){
-  setCount(count+1);
-}
+  // function handelClick() {
+  //   setCount(count + 1);
+  // }
   return (
-    <div>
-      <button incrementCount = {handelClick} text="click me">
-        <h1>{count}</h1>
-      </button>
-      
-      {/* <Card name="Suv">
+    //   <button incrementCount = {handelClick} text="click me">
+    //     <h1>{count}</h1>
+    //   </button>
+
+    <Card name="Suv">
         <h1>My name is suv</h1>
         <p>I am studing in CV Raman Global University</p>
-      </Card> */}
-    </div>
+    </Card> 
   )
 }
 
